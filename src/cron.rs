f853964d63b16ff7acab198ae8df7b9ba0baca42
LLSWE <tiilas777@gmail.com>
2026-04-26 21:25:21 -0300

    fix: leaks

--- src/cron.rs
+++
@@ -1,3 +0,0 @@
-use crate::model::ExternApiConfig;
-
-pub async fn checkup_contract(api_config: ExternApiConfig) {}
--- src/handler/get_contracts.rs
+++
@@ -1,9 +0,0 @@
-use crate::utils::get_token;
-use axum::extract::State;
-use std::sync::Arc;
-
-use crate::model::ExternApiConfig;
-
-pub async fn get_html_contracts(State(token_config): State<Arc<ExternApiConfig>>) {
-    let token = get_token(&token_config).await;
-}
